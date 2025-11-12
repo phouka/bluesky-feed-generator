2260dc7843767abe13db2d11b9908e5c02b81755
Your Name <you@example.com>
2025-11-11 19:03:48 -0800

    feed fix

--- server/algos/feed.py
+++ server/algos/feed.py
@@ -7,43 +7,55 @@ from server.database import Post, watch_list, ignore_list
 uri = config.FEED_URI
 CURSOR_EOF = 'eof'
 
+def compute_cursor(posts) -> str:
+    if not posts:
+        return CURSOR_EOF
+    last_post = posts[-1]
+    return f'{int(last_post.indexed_at.timestamp() * 1000)}::{last_post.cid}'
 
 def handler(cursor: Optional[str], limit: int) -> dict:
-    posts = Post.select().order_by(Post.cid.desc()).order_by(Post.indexed_at.desc()).limit(limit)
 
-    if cursor:
-        if cursor == CURSOR_EOF:
-            return {
-                'cursor': CURSOR_EOF,
-                'feed': []
-            }
-        cursor_parts = cursor.split('::')
-        if len(cursor_parts) != 2:
-            raise ValueError('Malformed cursor')
+    feed_posts = []
+    while True:
+        posts = Post.select().order_by(Post.cid.desc()).order_by(Post.indexed_at.desc()).limit(limit)
+        if cursor:
+            if cursor == CURSOR_EOF:
+                break
+            cursor_parts = cursor.split('::')
+            if len(cursor_parts) != 2:
+                raise ValueError('Malformed cursor')
 
-        indexed_at, cid = cursor_parts
-        indexed_at = datetime.fromtimestamp(int(indexed_at) / 1000)
-        posts = posts.where(((Post.indexed_at == indexed_at) & (Post.cid < cid)) | (Post.indexed_at < indexed_at))
+            indexed_at, cid = cursor_parts
+            indexed_at = datetime.fromtimestamp(int(indexed_at) / 1000)
+            posts = posts.where(((Post.indexed_at == indexed_at) & (Post.cid < cid)) | (Post.indexed_at < indexed_at))
+
+        
+        for post in posts:
+            url_parts = post.orig_uri.split('/')
+            orig_author = url_parts[2]
+
+            if orig_author in watch_list:
+                continue
+            
+            if orig_author in ignore_list:
+                continue
+
+            feed_posts.append(post)
+            if len(feed_posts) >= limit:
+                break
+
+        if len(feed_posts) >= limit:
+            break
+
+        cursor = compute_cursor(posts)
+
+    next_cursor = compute_cursor(feed_posts)
 
     feed = []
-    for post in posts:
-        url_parts = post.orig_uri.split('/')
-        orig_author = url_parts[2]
-
-        if orig_author in watch_list:
-            continue
-        
-        if orig_author in ignore_list:
-            continue
-
+    for post in feed_posts:
         feed.append({'post': post.orig_uri, "reason": { "$type": "app.bsky.feed.defs#skeletonReasonRepost", "repost": post.uri } })
 
-    cursor = CURSOR_EOF
-    last_post = posts[-1] if posts else None
-    if last_post:
-        cursor = f'{int(last_post.indexed_at.timestamp() * 1000)}::{last_post.cid}'
-
     return {
-        'cursor': cursor,
+        'cursor': next_cursor,
         'feed': feed
     }
