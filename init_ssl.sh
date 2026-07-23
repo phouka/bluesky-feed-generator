4f27a237996abc84aa8e3ead7449a6af180883f0
Your Name <you@example.com>
2026-07-22 17:04:00 -0700

    mkdir p

--- init_ssl.sh
+++ init_ssl.sh
@@ -23,6 +23,7 @@ fi
 
 if [ ! -e "$data_path/conf/options-ssl-nginx.conf" ] || [ ! -e "$data_path/conf/ssl-dhparams.pem" ]; then
     echo "### Downloading recommended TLS parameters ..."
+    mkdir -p "$data_path/conf"
     cp "./nginx/options-ssl-nginx.conf" "$data_path/conf/options-ssl-nginx.conf"
     cp "./nginx/ssl-dhparams.pem" "$data_path/conf/ssl-dhparams.pem"
 fi
